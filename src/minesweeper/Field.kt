8bc11553118e04b6263df5ecf764d217df911b52
steleal <steleal@yandex.ru>
2020-01-24 13:16:40 +0400

    Refactoring of Field: updated functions

--- src/minesweeper/Field.kt
+++ src/minesweeper/Field.kt
@@ -6,21 +6,50 @@ class Field(val size: Int = 9, val mines: Int = 10) {
     private val last = size - 1
     private var countOfMine = 0
 
-    private val BLANK = "." //for printing
-    private val MINE = "X" //for printing
-    private val MINE_NUM = -1  //for keeping in array of cells
-
-    private val cells = Array(size) { IntArray(size) { 0 } }
+    private val cells = Array(size) { Array(size) { Cell(CellValue.EMPTY) } }
 
     init {
         while (countOfMine < mines) {
             val row = Random.nextInt(size)
             val column = Random.nextInt(size)
-            addMine(row, column)
+            if (!addMine(row, column)) continue
+            increaseCounterforEachNeighbor(row, column)
+            countOfMine++
         }
     }
 
-    fun allToString(): String {
+    fun existsCell(row: Int, column: Int): Boolean {
+        return row in 0..last
+                && column in 0..last
+    }
+
+    fun isOpenCell(row: Int, column: Int): Boolean {
+        return cells[row][column].isOpen()
+    }
+
+    private fun addMine(row: Int, column: Int): Boolean {
+        if (!existsCell(row, column)) return false
+        if (cells[row][column].isMine()) return false
+
+        cells[row][column].value = CellValue.MINE
+        return true
+    }
+
+    private fun increaseCounterforEachNeighbor(row: Int, column: Int) {
+        val neighbors = arrayOf(Pair(-1, -1), Pair(-1, 0), Pair(-1, 1), Pair(0, -1), Pair(0, 1), Pair(1, -1), Pair(1, 0), Pair(1, 1))
+
+        neighbors.forEach {
+            val neighborRow = row + it.first
+            val neighborColumn = column + it.second
+
+            if (existsCell(neighborRow, neighborColumn) &&
+                    !cells[neighborRow][neighborColumn].isMine()) {
+                cells[neighborRow][neighborColumn].value = CellValue.MINE
+            }
+        }
+    }
+}
+/*    fun allToString(): String {
         val printField = StringBuilder()
         cells.forEach {
             it.joinTo(printField, "") {
@@ -34,38 +63,4 @@ class Field(val size: Int = 9, val mines: Int = 10) {
         }
         return printField.toString()
     }
-
-    private fun addMine(row: Int, column: Int): Boolean {
-        if (!existCells(row, column)) return false
-        if (isMine(row, column)) return false
-
-        cells[row][column] = MINE_NUM
-        countOfMine++
-        increaseCounterforEachNeighbor(row, column)
-        return true
-    }
-
-    private fun isMine(row: Int, column: Int): Boolean {
-        return cells[row][column] == MINE_NUM
-    }
-
-    private fun existCells(row: Int, column: Int): Boolean {
-        return row in 0..last
-                && column in 0..last
-    }
-
-    private fun increaseCounterforEachNeighbor(row: Int, column: Int) {
-        val neighbors = arrayOf(Pair(-1, -1), Pair(-1, 0), Pair(-1, 1), Pair(0, -1), Pair(0, 1), Pair(1, -1), Pair(1, 0), Pair(1, 1))
-
-        neighbors.forEach {
-            val neighborRow = row + it.first
-            val neighborColumn = column + it.second
-            if (existCells(neighborRow, neighborColumn) &&
-                    cells[neighborRow][neighborColumn] != MINE_NUM) {
-
-                cells[neighborRow][neighborColumn]++
-            }
-        }
-    }
-
-}
+*/
