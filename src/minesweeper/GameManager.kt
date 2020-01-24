151f6c87fe0634bfbb90bc0a5ab37fac89687e25
steleal <steleal@yandex.ru>
2020-01-24 15:56:18 +0400

    Implement fun nextStep and printField in GameManager.kt

--- src/minesweeper/GameManager.kt
+++ src/minesweeper/GameManager.kt
@@ -1,14 +1,56 @@
 package minesweeper
 
-class GameManager(val field: Field) {
+import java.util.Scanner
+
+class GameManager(val field: Field, val scanner: Scanner) {
+    var over: Boolean = false
+    var win: Boolean = false
+
     fun printField() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        println(field.openedToString())
     }
 
     fun nextStep() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val (x, y) = askMarkCoordinates()
+        val cell = field.getCell(x, y) ?: return
+        cell.mineFlag = !cell.mineFlag
+        win = checkWinState(field)
+        over = win//TODO checkEndOfGame()
     }
 
-    var over: Boolean = false
-    var win: Boolean = false
+    private fun checkWinState(field: Field): Boolean {
+        field.cells.forEach {
+            it.forEach { cell ->
+                if (cell.mineFlag != cell.mine) return false
+            }
+        }
+        return true
+    }
+
+    fun askMarkCoordinates(): Pair<Int, Int> {
+        var x = -1
+        var y = -1
+        while (!isAcceptable(x, y)) {
+            print("Set/delete mines marks (x and y coordinates): ")
+            x = scanner.nextInt()
+            y = scanner.nextInt()
+            printIfOpenedCell(field.getCell(x, y))
+        }
+        return Pair(x, y)
+    }
+
+    private fun printIfOpenedCell(cell: Cell?) {
+        cell ?: return
+        if (cell.open) {
+            when (cell.cntOfmineInNeighbors) {
+                0 -> println("Here is a open cell!")
+                else -> println("There is a number here!")
+            }
+        }
+    }
+
+    fun isAcceptable(x: Int, y: Int): Boolean {
+        return field.existsCell(x, y) &&
+                !field.isOpenCell(x, y)
+    }
 }
