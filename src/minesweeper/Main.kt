20b1e529fad6756ad60240af55a092a0e1a80cbc
steleal <steleal@yandex.ru>
2020-01-24 11:18:48 +0400

    Updated Main.kt: added game loop, fun initField, print end game message

--- src/minesweeper/Main.kt
+++ src/minesweeper/Main.kt
@@ -5,10 +5,33 @@ import java.util.Scanner
 fun main() {
     val scanner = Scanner(System.`in`)
 
+    val field = initField(scanner)
+    val game = GameManager(field)
+
+    while (!game.over) {
+        game.printField()
+        game.nextStep()
+    }
+
+    when {
+        game.win -> congratulation()
+        else -> sorry()
+    }
+
+}
+
+private fun sorry() {
+    println("You lose!")
+}
+
+private fun congratulation() {
+    println("Congratulations! You found all mines!")
+}
+
+private fun initField(scanner: Scanner): Field {
     println("How many mines do you want on the field?")
-    val amountOfMines = scanner.nextInt()
-
-    val field = Field(9, amountOfMines)
-
-    print(field.allToString())
+    return Field(
+            size = 9,
+            mines = scanner.nextInt()
+    )
 }
