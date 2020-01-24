7a1c57c3b6c1ec10710f5a845bf7820495e5ba3a
steleal <steleal@yandex.ru>
2020-01-24 11:15:39 +0400

    Added GameManager.kt

---
+++ src/minesweeper/GameManager.kt
@@ -0,0 +1,14 @@
+package minesweeper
+
+class GameManager(val field: Field) {
+    fun printField() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun nextStep() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    var over: Boolean = false
+    var win: Boolean = false
+}
